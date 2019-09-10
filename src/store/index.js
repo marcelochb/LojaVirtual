41928e8468c12a38e370082216414fca78bc9eb9
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-10 10:14:27 -0300

    configurando redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
 const enhacer =
   process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
